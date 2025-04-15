7cad771cf78ca30f13c2637f1e4842c21f5dcd67
berz20 <matteo.barbagiovanni@edu.unito.it>
2025-04-15 10:43:02 +0200

    Vertical workspace 3 fingers

--- src/scroller.cpp
+++ src/scroller.cpp
@@ -1502,25 +1502,12 @@ void ScrollerLayout::swipe_update(SCallbackInfo &info,
   if (**SENABLE && swipe_event.fingers == **SFINGERS) {
     if (s == nullptr)
       return;
-    if (std::abs(gesture_delta.x) > std::abs(gesture_delta.y)) {
+    if (std::abs(gesture_delta.x) > std::abs(gesture_delta.y))
       swipe_direction =
           gesture_delta.x > 0 ? Direction::Right : Direction::Left;
-      s->scroll_update(swipe_direction, delta);
-    } else {
-      // swipe_direction = gesture_delta.y > 0 ? Direction::Down :
-      // Direction::Up;
-      if (swipe_active)
-        return;
-      if (gesture_delta.y <= -**WDISTANCE) {
-        std::string offset(*WPREFIX);
-        g_pKeybindManager->m_mDispatchers["workspace"](
-            **HSINVERT ? offset + "+1" : offset + "-1");
-      } else if (gesture_delta.y >= **WDISTANCE) {
-        std::string offset(*WPREFIX);
-        g_pKeybindManager->m_mDispatchers["workspace"](
-            **HSINVERT ? offset + "-1" : offset + "+1");
-      }
-    }
+    else
+      swipe_direction = gesture_delta.y > 0 ? Direction::Down : Direction::Up;
+    s->scroll_update(swipe_direction, delta);
   } else {
     // Undo natural
     const Vector2D delta = gesture_delta * (**NATURAL ? -1.0 : 1.0);
